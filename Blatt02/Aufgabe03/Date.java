4bb041b4902c5a3fa63315680b4b9667e23487cc
Marco5de <marco.deuscher@t-online.de>
2018-04-29 10:46:43 +0200

    added comment

--- Blatt02/Aufgabe03/Date.java
+++ Blatt02/Aufgabe03/Date.java
@@ -1,13 +1,21 @@
 import java.time.LocalDate;
 
+/**
+ * Implementation of a),b) and c)
+ * prints today's date
+ * prints the date four weeks ago and the date one month ago
+ * prints the associated day of the week
+ * @author Deuscher Marco
+ * @author Jutz Benedikt
+ */
 public class Date {
     public static void main(String[]args){
         LocalDate date = LocalDate.now();
         System.out.println(date);
 
-
-        //Sind unterschiedlich da vor vier Wochen heutigesDatum - 4 Wochen = heutigesDatum -28 Tage
-        // und heutigesDatum - 1Monat 
+        //the two dates differ, because four weeks ago ago is equivalent to:
+        // todays date - 4 weeks = today's date - 28 days
+        //todays date - 1 month = day - 0; month -1
         System.out.println(date.minusWeeks(4));
         System.out.println(date.minusMonths(1));
 
